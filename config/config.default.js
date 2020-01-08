ba83be1cf716c5b7ea894fdff441e6e975664c16
caiwenduo <caiwenduo@163.com>
2020-01-08 14:56:04 +0800

    add news

--- config/config.default.js
+++ config/config.default.js
@@ -17,59 +17,10 @@ module.exports = appInfo => {
 
   // add your middleware config here
   config.middleware = []
-  // 设置端口
-  config.cluster = {
-    listen: {
-      path: '',
-      port: 8008,
-      hostname: '0.0.0.0'
-    }
-  }
-  // mongodb 数据库设置
-  config.mongoose = {
-    client: {
-      url: 'mongodb://47.98.50.170/web-blog-dep',
-      useNewUrlParser: true,
-      options: {
-        mongos: true,
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-        // autoIndex: false,
-        // replicaSet: 'rs0',
-        // readPreference: 'secondary',
-        // w: 'majority',
-        // authSouce: 'admin'
-      }
-    }
-  }
   config.view = {
     mapping: {
       '.html': 'nunjucks'
     }
-  }
-  // 设置redis
-  config.redis = {
-    // 单个redis
-    client: {
-      host: '47.98.50.170', // Redis host
-      port: 6379, // Redis port
-      password: 'caiwenduo1993',
-      db: 1
-    }
-  }
-  // 设置session
-  config.session = {
-    key: 'SESSION_ID',
-    maxAge: 60000, // 1 天
-    httpOnly: true, // 设置键值对是否可以被 js 访问，默认为 true，不允许被 js 访问。
-    encrypt: true, // 加密传输
-    renew: true // 延长会话有效期
-  }
-  // 七牛云的配置
-  config.qiniuyun = {
-    accessKey: 'y2suiyW_lcVyb-E1RkJiL-g4WiBMN3f69QjrbtKd',
-    secretKey: '4AgJMyTfmQggOsa2jnCPXvlJInJGNXS1Irm2pfHY',
-    bucket: 'caiwenduo' // 空间名称
   }
   // 配置需要的中间件，数组顺序即为中间件的加载顺序
   config.middleware = ['login', 'errorHandler']
---
+++ config/config.prod.js
@@ -0,0 +1,65 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict'
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = (exports = {})
+
+  // add your middleware config here
+  config.middleware = []
+  // 设置端口
+  config.cluster = {
+    listen: {
+      path: '',
+      port: 8008,
+      hostname: '0.0.0.0'
+    }
+  }
+  // mongodb 数据库设置
+  config.mongoose = {
+    client: {
+      url: 'mongodb://39.105.181.82/web-blog-dep',
+      useNewUrlParser: true,
+      options: {
+        mongos: true,
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+      }
+    }
+  }
+  // 设置redis
+  config.redis = {
+    // 单个redis
+    client: {
+      host: '39.105.181.82', // Redis host
+      port: 6379, // Redis port
+      password: 'caiwenduo1993',
+      db: 1
+    }
+  }
+  // 设置session
+  config.session = {
+    key: 'SESSION_ID',
+    maxAge: 60000, // 1 天
+    httpOnly: true, // 设置键值对是否可以被 js 访问，默认为 true，不允许被 js 访问。
+    encrypt: true, // 加密传输
+    renew: true // 延长会话有效期
+  }
+  // 七牛云的配置
+  config.qiniuyun = {
+    accessKey: 'y2suiyW_lcVyb-E1RkJiL-g4WiBMN3f69QjrbtKd',
+    secretKey: '4AgJMyTfmQggOsa2jnCPXvlJInJGNXS1Irm2pfHY',
+    bucket: 'caiwenduo' // 空间名称
+  }
+  return {
+    ...config,
+    ...userConfig
+  }
+}
