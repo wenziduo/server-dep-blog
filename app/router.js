c78c5fd33b3320d525a5b6082347ecb7262011d7
caiwenduo <caiwenduo@163.com>
2020-01-09 16:56:45 +0800

    add

--- app/router.js
+++ app/router.js
@@ -5,16 +5,16 @@
  */
 module.exports = app => {
   const { router, controller } = app
-  router.get('/home', controller.home.index)
-  router.get('/home/work', controller.home.work)
-  router.post('/user/login', controller.user.login)
-  router.get('/classify/find', controller.classify.find) // 查找分类
-  router.post('/classify/create', controller.classify.create) // 创建分类
-  router.post('/classify/update', controller.classify.update) // 编辑分类
-  router.post('/classify/delete', controller.classify.delete) // 删除分类
-  router.post('/post/create', controller.post.create) // 创建文章
-  router.get('/post/find', controller.post.find) // 文章列表
-  router.get('/post/detail', controller.post.detail) // 文章详情
-  router.get('/post/test', controller.post.test) // 文章详情
-  router.get('/qiniu/getQiniuToken', controller.qiniu.getQiniuToken) // 文获取七牛token
+  router.get('/api/home', controller.home.index)
+  router.get('/api/home/work', controller.home.work)
+  router.post('/api/user/login', controller.user.login)
+  router.get('/api/classify/find', controller.classify.find) // 查找分类
+  router.post('/api/classify/create', controller.classify.create) // 创建分类
+  router.post('/api/classify/update', controller.classify.update) // 编辑分类
+  router.post('/api/classify/delete', controller.classify.delete) // 删除分类
+  router.post('/api/post/create', controller.post.create) // 创建文章
+  router.get('/api/post/find', controller.post.find) // 文章列表
+  router.get('/api/post/detail', controller.post.detail) // 文章详情
+  router.get('/api/post/test', controller.post.test) // 文章详情
+  router.get('/api/qiniu/getQiniuToken', controller.qiniu.getQiniuToken) // 文获取七牛token
 }
