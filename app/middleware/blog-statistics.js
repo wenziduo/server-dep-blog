91bf516fc3f2ace48d09b0eee59498b71a1f8f4b
caiwenduo <cawenduo@163.com>
2020-03-25 21:34:47 +0800

    add houduan

--- app/middleware/blog-statistics.js
+++ app/middleware/blog-statistics.js
@@ -3,7 +3,7 @@
 module.exports = () => {
   return async function(ctx, next) {
     const { path } = ctx.request;
-    if (path.search('/blog') !== -1) {
+    if (path.search('/blog') !== -1 || path.search('/admin-blog') !== -1) {
       await ctx.service.statistics.create({ type: 0 });
     }
     await next();
